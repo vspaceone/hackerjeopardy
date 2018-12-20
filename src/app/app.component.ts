63d7a3ef58ec320b3cc838e5923116d1961189c3
noppelmax <gitkraken@noppelmax.online>
2018-12-20 15:53:19 +0100

    Show images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,10 @@ export class AppComponent {
     this.selectedQuestion = undefined
   }
 
+  cancel(): void {
+    this.selectedQuestion = undefined
+  }
+
   category = [
     "ports","ports","ports","ports","ports"
   ]
@@ -44,7 +48,7 @@ export class AppComponent {
 
   qanda = [[
       {"question":"Welchen Port hat SSH?333", "answer": "223", "value":100,"available":true},
-      {"question":"Welchen Port hat SSH?", "answer": "22", "value":200,"available":true},
+      {"question":"Welchen Port hat SSH?", "image": "https://www.gettyimages.com/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg", "value":200,"available":true},
       {"question":"Welchen Port hat SSH?", "answer": "22", "value":300,"available":true},
       {"question":"Welchen Port hat SSH?", "answer": "22", "value":400,"available":true},
       {"question":"Welchen Port hat SSH?", "answer": "22", "value":500,"available":true},

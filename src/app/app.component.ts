6995664f4ff7cf414c5a24fe6ace6e3659c44052
noppelmax <gitkraken@noppelmax.online>
2018-12-20 15:17:53 +0100

    Set unavailable when clicked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
+
+  onSelect(q): void {
+    console.log("Hallo onSelect", q)
+    q.available = false
+  }
+
   category = [
     "ports","ports","ports","ports","ports"
   ]
@@ -15,11 +21,11 @@ export class AppComponent {
   players = [
     {"name":"max", "score": 0},
     {"name":"nils", "score": 0},
-    {"name":"tom", "score": 0}    
+    {"name":"tom", "score": 0}
   ]
 
   qanda = [[
-      {"question":"Welchen Port hat SSH?", "answer": "22", "value":100,"available":true},
+      {"question":"Welchen Port hat SSH?333", "answer": "223", "value":100,"available":true},
       {"question":"Welchen Port hat SSH?", "answer": "22", "value":200,"available":true},
       {"question":"Welchen Port hat SSH?", "answer": "22", "value":300,"available":true},
       {"question":"Welchen Port hat SSH?", "answer": "22", "value":400,"available":true},

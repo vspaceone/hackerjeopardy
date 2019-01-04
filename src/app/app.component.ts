486ad1cb0189ed76cb5472b073192e15628f9744
noppelmax <gitkraken@noppelmax.online>
2019-01-04 18:24:17 +0100

    Load different sets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+declare var jquery:any;
+declare var $ :any;
 
 
 @Component({
@@ -9,6 +12,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
+  constructor(private http: HttpClient) { };
 
   selectedQuestion = undefined
   renamePlayer = undefined
@@ -31,6 +35,12 @@ export class AppComponent {
     }
     q.available = false
     this.couldBeCanceled = false;
+  }
+
+  selectSet(s): void {
+    this.http.get("/assets/"+s).subscribe(data => {
+      this.qanda = data;
+    })
   }
 
   notanswered(q,p): void {
@@ -75,176 +85,7 @@ export class AppComponent {
     {"name":"player3", "score": 0}
   ]
 
-  qanda = [
-    {
-    "category":"Crypto",
-    "questions":[
-    {
-      "available": true,
-      "value": 100,
-      "question": "This simple cipher shifts each character by a given value",
-      "answer": "Caesar cipher"
-    },
-    {
-      "available": true,
-      "value": 200,
-      "question": "This was the effective keysize of the DES cipher",
-      "answer": "56 bits (7 bytes)"
-    },
-    {
-      "available": true,
-      "value": 300,
-      "question": "This country was the first to break the Enigma machine",
-      "answer": "Poland (at the Polish Cipher Bureau)"
-    },
-    {
-      "available": true,
-      "value": 400,
-      "question": "The AES standards defines this many possible block sizes",
-      "answer": "1 (128 bits)"
-    },
-    {
-      "available": true,
-      "value": 500,
-      "question": "This cipher was used incorrectly in WEP",
-      "answer": "RC4"
-    }
-  ]},
-  {
-  "category":"Famous Hackers",
-  "questions":[
-      {
-        "available": true,
-        "value": 100,
-        "question": "This is the core maintainer of the Linux operating system",
-        "answer": "Linus Torvalds"
-      },
-      {
-        "available": true,
-        "value": 200,
-        "question": "Rumours that he could launch nuclear missiles by whistling into the phone caused this man to be left in solitary confinement for 8 months",
-        "answer": "Kevin Mitnick"
-      },
-      {
-        "available": true,
-        "value": 300,
-        "question": "John \"Captain Crunch\" Draper discovered that toy whistles from the Captain Crunch box blew this very important frequency",
-        "answer": "2600 hertz"
-      },
-      {
-        "available": true,
-        "value": 400,
-        "question": "This man wrote - and shares his name with - the first (known) computer worm in history",
-        "answer": "Robert Morris"
-      },
-      {
-        "available": true,
-        "value": 500,
-        "question": "This man helped the FBI capture Kevin Mitnick",
-        "answer": "Tsutomu Shimomura"
-  }
-]},
-{
-"category":"Simpsons",
-"questions":[
-      {
-        "available": true,
-        "value": 100,
-        "question": "He's the all-too-friendly next door neighbour",
-        "answer": "Ned Flanders"
-      },
-      {
-        "available": true,
-        "value": 200,
-        "question": "This is the name of the Simpsons family's dog",
-        "answer": "Santa's Little Helper"
-      },
-      {
-        "available": true,
-        "value": 300,
-        "question": "In the famous two-part episode, this person was the one who shot Mr Burns",
-        "answer": "Maggie Simpson"
-      },
-      {
-        "available": true,
-        "value": 400,
-        "question": "These are the names of the three main bullies (not counting Nelson)",
-        "answer": "Jimbo, Kearney, and Dolph (or \"James Jones\", \"Kevin Zzyzwicz\", and \"Darren Starbeam\")"
-      },
-      {
-        "available": true,
-        "value": 500,
-        "question": "This toy, released by Kid First Industries, was programmed to attack other toys",
-        "answer": "Funzo"
-      }
-    ]},
-    {
-    "category":"Hackermovies",
-    "questions":[
-      {
-        "available": true,
-        "value": 100,
-        "question": "This movie popularized the phrase \"hack the Gibson\".",
-        "answer": "Hackers"
-      },
-      {
-        "available": true,
-        "value": 200,
-        "question": "This movie featured breaking into a government agency during fellatio.",
-        "answer": "Swordfish"
-      },
-      {
-        "available": true,
-        "value": 300,
-        "question": "This movie contained the famous line \"the only winning move is not to play\".",
-        "answer": "WarGames"
-      },
-      {
-        "available": true,
-        "value": 400,
-        "question": "This movie's was based on a novel titled \"Men Who Hate Women\".",
-        "answer": "Girl with the Dragon Tattoo"
-      },
-      {
-        "available": true,
-        "value": 500,
-        "question": "This movie featured the memorable phrase \"My voice is my passport\".",
-        "answer": "Sneakers"
-      }
-    ]},
-    {
-    "category":"Exploits",
-    "questions":[
-      {"available": true,
-        "value": 100,
-        "question": "This exploit occurs when more data is written than memory is allocated.",
-        "answer": "Buffer overflow"
-      },
-      {
-        "available": true,
-        "value": 200,
-        "question": "This exploit involves finding gadgets to bypass a non-executable stack.",
-        "answer": "Return-oriented programming (or return-to-libc)"
-      },
-      {
-        "available": true,
-        "value": 300,
-        "question": "This exploit permits an attacker to add data to the end of a hash without knowing all data that produced it.",
-        "answer": "A hash length extension attack"
-      },
-      {
-        "available": true,
-        "value": 400,
-        "question": "This attack involves deriving information from timing or power consumption information.",
-        "answer": "Sidechannel attack"
-      },
-      {
-        "available": true,
-        "value": 500,
-        "question": "This attack involves logging into a service with a hashed password instead of the password itself.",
-        "answer": "Pass the hash"
-      }
-    ]}
-  ];
+  qanda = undefined;
+  sets = ["xmas18_rnd1.json", "xmas18_rnd2.json"];
 
 }

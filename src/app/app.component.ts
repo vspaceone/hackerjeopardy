3b2ef7d2553ae9314ce760d04b5698daf57f6881
noppelmax <max@noppelmax.online>
2019-12-20 15:35:37 +0100

    Add new turns for XMAS19 Implement music fg and bg color fixes for different path and name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,7 @@ export class AppComponent {
 
 
 	onSelect(q): void {
+		$('#audiotheme').trigger('play')
 		console.log("Hallo onSelect", q)
 		this.selectedQuestion = q
 		this.couldBeCanceled = true;
@@ -26,6 +27,8 @@ export class AppComponent {
 
 	answered(q,p): void {
 		console.log(q)
+		$('#audiotheme').trigger('pause')
+		$('#audiotheme').trigger('load')
 
 		if(!p){
 
@@ -33,19 +36,24 @@ export class AppComponent {
 			p.score = p.score + this.selectedQuestion.value;
 		}
 		q.available = false;
-		q.color = p.color;
+		q.bgcolor = p.bgcolor;
+		q.fgcolor = p.fgcolor;
 		this.couldBeCanceled = false;
 	}
 
 	selectSet(s): void {
 		this.http.get("/assets/"+s+"/turn.json").subscribe(data => {
 			this.qanda = []
-			for( var i = 0; i <= data["categories"].length; i ++){
+			for( var i = 0; i <= data["categories"].length-1; i ++){
 				this.http.get("/assets/"+s+"/"+data["categories"][i]+"/cat.json").subscribe(cat => {
 					for( var qIdx = 0; qIdx < cat["questions"].length; qIdx++){
 						cat["questions"][qIdx].available = true;
 						cat["questions"][qIdx].value = (qIdx + 1) * 100;
-						if(cat["questions"][qIdx]["image"]){
+						cat["questions"][qIdx].cat = cat["name"]
+						if(cat["questions"][qIdx]["image"] && cat["path"]){
+							cat["questions"][qIdx]["image"] = "assets/"+s+"/"+cat["path"]+"/"+cat["questions"][qIdx]["image"]
+						}
+						else if(cat["questions"][qIdx]["image"] && cat["name"]){
 							cat["questions"][qIdx]["image"] = "assets/"+s+"/"+cat["name"]+"/"+cat["questions"][qIdx]["image"]
 						}
 					}
@@ -57,6 +65,7 @@ export class AppComponent {
 				for( var qIdx = 0; qIdx < (this.qanda[catIdx].questions.length); qIdx++){
 					this.qanda[catIdx].questions[qIdx].available = true;
 					this.qanda[catIdx].questions[qIdx].value = (qIdx + 1) * 100;
+					
 				}
 			}
 		});
@@ -65,6 +74,8 @@ export class AppComponent {
 
 	notanswered(q,p): void {
 		console.log(q)
+		$('#audiotheme').trigger('pause')
+		$('#audiotheme').trigger('load')
 
 		if(!p){
 
@@ -84,10 +95,14 @@ export class AppComponent {
 	}
 
 	close(): void {
+		$('#audiotheme').trigger('pause')
+		$('#audiotheme').trigger('load')
 		this.selectedQuestion = undefined
 	}
 
 	cancel(): void {
+		$('#audiotheme').trigger('pause')
+		$('#audiotheme').trigger('load')
 		this.selectedQuestion = undefined
 	}
 
@@ -100,18 +115,20 @@ export class AppComponent {
 	}
 
 	players = [
-		{"name":"player1", "score": 0, "color": "#ff6b6b"},
-		{"name":"player2", "score": 0, "color": "#6eff6b"},
-		{"name":"player3", "score": 0, "color": "#9cfcff"}
+		{"name":"player1", "score": 0, "bgcolor": "#ff6b6b", "fgcolor": "#9f0b0b"},
+		{"name":"player2", "score": 0, "bgcolor": "#6eff6b", "fgcolor": "#0e9f0b"},
+		{"name":"player3", "score": 0, "bgcolor": "#9cfcff", "fgcolor": "#3c9c9f"}
 	]
 
 	qanda = undefined;
 	sets = [
+		"XMAS19-Turn1",
+		"XMAS19-Turn2",
+		"XMAS19-Turn3",
 		"Lounge_And_Chill_1",
 		"Lounge_And_Chill_2",
 		"Lounge_And_Chill_3",
 		"Tim_Runde",
-		"Turn1",
 		"XMAS18_RND1",
 		"XMAS18_RND2"
 	];
